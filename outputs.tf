5f2bb7df2a5e2906d9af8d35361220b02f851ae1
William Konitzer <william@konitzer.co.uk>
2023-09-25 15:05:51 -0500

    add hosts output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "mke_cluster" {
   value = yamlencode(local.launchpad_tmpl)
+  description = "The launchpad template in yaml format"
+}
+
+output "hosts" {
+  value       = concat(local.managers, local.workers)
+  description = "All hosts in the cluster"
 }
